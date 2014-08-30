76aceb717bd0138e185417693b7c05e1f8dcb465
Ben Chatelain <benchatelain@gmail.com>
2014-08-30 07:18:50 -0600

    Remove unused RGBFlipsideViewController

--- RoyGBeacon/RGBMainViewController.h
+++ RoyGBeacon/RGBMainViewController.h
@@ -6,10 +6,9 @@
 //  Copyright (c) 2013 Ben Chatelain. All rights reserved.
 //
 
-#import "RGBFlipsideViewController.h"
 #import <CoreLocation/CoreLocation.h>
 
-@interface RGBMainViewController : UIViewController <RGBFlipsideViewControllerDelegate, UIPopoverControllerDelegate, CLLocationManagerDelegate>
+@interface RGBMainViewController : UIViewController <CLLocationManagerDelegate>
 
 @property (strong, nonatomic) UIPopoverController *flipsidePopoverController;
 
--- RoyGBeacon/RGBMainViewController.m
+++ RoyGBeacon/RGBMainViewController.m
@@ -126,43 +126,4 @@
     }
 }
 
-#pragma mark - Flipside View Controller
-
-- (void)flipsideViewControllerDidFinish:(RGBFlipsideViewController *)controller
-{
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        [self dismissViewControllerAnimated:YES completion:nil];
-    } else {
-        [self.flipsidePopoverController dismissPopoverAnimated:YES];
-    }
-}
-
-- (void)popoverControllerDidDismissPopover:(UIPopoverController *)popoverController
-{
-    self.flipsidePopoverController = nil;
-}
-
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    if ([[segue identifier] isEqualToString:@"showAlternate"]) {
-        [[segue destinationViewController] setDelegate:self];
-        
-        if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
-            UIPopoverController *popoverController = [(UIStoryboardPopoverSegue *)segue popoverController];
-            self.flipsidePopoverController = popoverController;
-            popoverController.delegate = self;
-        }
-    }
-}
-
-- (IBAction)togglePopover:(id)sender
-{
-    if (self.flipsidePopoverController) {
-        [self.flipsidePopoverController dismissPopoverAnimated:YES];
-        self.flipsidePopoverController = nil;
-    } else {
-        [self performSegueWithIdentifier:@"showAlternate" sender:sender];
-    }
-}
-
 @end
