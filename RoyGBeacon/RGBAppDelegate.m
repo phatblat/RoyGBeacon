acd69a4b62cde969cc1fc9f5590f939d3ed17cd9
Ben Chatelain <benchatelain@gmail.com>
2014-08-30 08:12:51 -0600

    Add labels to iPad storyboard

--- RoyGBeacon/RGBAppDelegate.m
+++ RoyGBeacon/RGBAppDelegate.m
@@ -44,10 +44,4 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
-#pragma mark - CLLocationManagerDelegate
-
-//- (void)locationManager:(CLLocationManager *)manager didDetermineState:(CLRegionState)state forRegion:(CLRegion *)region
-//{
-//}
-
 @end
